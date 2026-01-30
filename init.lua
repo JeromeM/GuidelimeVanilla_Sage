81c2d810f4d5ebf29e9d238a25e7a38abe5df582
JeromeM <meyer.jerome@gmail.com>
2026-01-30 23:20:21 +0100

    Add Sage leveling guide

---
+++ init.lua
@@ -0,0 +1,18 @@
+--[[
+    GuidelimeVanilla_Sage
+    Sage 1-60 Alliance leveling guides for Guidelime Vanilla
+
+    Website: sageguide.net
+]]--
+
+local GLV = LibStub("GuidelimeVanilla")
+if not GLV then
+    DEFAULT_CHAT_FRAME:AddMessage("|cFFFF0000[Sage Guide]|r GuidelimeVanilla is required!")
+    return
+end
+
+-- Register addon name for metadata lookup
+GLV.guidePackAddons = GLV.guidePackAddons or {}
+GLV.guidePackAddons["Sage Guide"] = "GuidelimeVanilla_Sage"
+
+DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00[Sage Guide]|r Loaded successfully")
